d0700702e22fe3b7cec102906c211ae374fd967c
LuLuCheeky <lulucheekypro@gmail.com>
2025-08-31 08:56:56 +0000

    Aug 31, 2025, 8:56 AM

--- testquestion2.js
+++ testquestion2.js
@@ -10,6 +10,7 @@ function setup(){
 
 function draw(){
     for (let count = 1; count < 11; count++) {
+        fill()
         circle(x, y, 50)
         x = fixedX * count
         y = fixedY * count
