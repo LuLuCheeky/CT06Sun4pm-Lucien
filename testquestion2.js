d0ed42d6fa292bd2fbea3ac29042a4766df7cd4c
LuLuCheeky <lulucheekypro@gmail.com>
2025-08-31 08:57:36 +0000

    Aug 31, 2025, 8:57 AM

--- testquestion2.js
+++ testquestion2.js
@@ -2,7 +2,7 @@ let x = 50
 let y = 50
 let fixedX = 50
 let fixedY = 50
-let colour = 225
+let colour = 255
 function setup(){
     createCanvas(600, 600);
     background("grey");
@@ -10,7 +10,7 @@ function setup(){
 
 function draw(){
     for (let count = 1; count < 11; count++) {
-        fill(colour, colour, colour)
+        fill(colour)
         circle(x, y, 50)
         x = fixedX * count
         y = fixedY * count
